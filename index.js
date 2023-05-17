ecd5f3d837df340dc9b5f76e45c89e6c85420385
sadiawcutecat <98077710+sadiawcutecat@users.noreply.github.com>
2023-05-18 00:33:49 +0600

    server side is running

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+
+ app.get('/', (req, res)=>{
+    res.send('server is running')
+ })
+
+ app.listen(port, ()=>{
+    console.log(`toy server is running: ${port}`);
+ })
