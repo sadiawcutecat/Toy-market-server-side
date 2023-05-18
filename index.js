cc287ec28054b16a1078bd6a195649ae76d6a3ae
sadiawcutecat <98077710+sadiawcutecat@users.noreply.github.com>
2023-05-18 15:02:15 +0600

    a json file added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const customerComment = require('./Data/cutommerComment.json');
 
  app.get('/', (req, res)=>{
     res.send('server is running')
