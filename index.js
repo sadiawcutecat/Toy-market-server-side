3ebc20ac0113d52aa3031062b57e2504d114315b
sadiawcutecat <98077710+sadiawcutecat@users.noreply.github.com>
2023-05-18 15:14:44 +0600

    data provided to client side

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 const customerComment = require('./Data/cutommerComment.json');
 
+app.use(cors());
+
  app.get('/', (req, res)=>{
     res.send('server is running')
+ })
+ app.get('/customerComment',(req, res)=>{
+   res.send(customerComment);
+   console.log(customerComment);
  })
 
  app.listen(port, ()=>{
