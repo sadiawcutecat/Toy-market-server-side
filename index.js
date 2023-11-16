8cb9a9addb422bf0b8de8f13626d59f10bb661bb
sadiawcutecat <98077710+sadiawcutecat@users.noreply.github.com>
2023-11-16 20:16:11 +0600

    toy-market-server-side

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json()); // Add this line to enable body parsing
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s1xtzuq.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -35,15 +35,67 @@ async function run() {
           query = { email: req.query.email }
       }
       const result = await toysCollection.find(query).toArray();
+      console.log(result);
       res.send(result);
   })
 
-  app.post('/api/saveData',async (req, res) => {
+  app.post('/api/toys',async (req, res) => {
     const toys = req.body;
     console.log(toys);
     const result = await toysCollection.insertOne(toys);
     res.send(result);
 });
+
+app.get('/api/allToys', async(req, res) =>{
+  console.log(req.body);
+  const result = await toysCollection.find({}).toArray();
+  console.log(result);
+  res.send(result);
+})
+
+// app.get('/api/myToys', async(req, res) =>{
+//   const { email } = req.query;
+//   console.log(email);
+//   const result = await toysCollection.find({}).toArray();
+//   console.log(result);
+//   res.send(result);
+// })
+
+app.get('/api/myToys', async (req, res) => {
+  try {
+    const em = req.params.email;
+    console.log(em);
+  
+    const result = await toysCollection.findOne({ email: em });
+    console.log(result);
+  
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.put('/api/myToys/:id', async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const updateToys = req.body;
+  console.log(updateToys);
+  const updateDoc = {
+      $set: {
+          status: updateToys.status
+      },
+  };
+  const result = await toysCollection.updateOne(filter, updateDoc);
+  res.send(result);
+})
+
+app.delete('/api/myToys/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) }
+  const result = await toysCollection.deleteOne(query);
+  res.send(result);
+})
 
 
 
@@ -65,7 +117,7 @@ run().catch(console.dir);
  })
  app.get('/customerComment',(req, res)=>{
    res.send(customerComment);
-   console.log(customerComment);
+  //  console.log(customerComment);
  })
 
  app.listen(port, ()=>{
